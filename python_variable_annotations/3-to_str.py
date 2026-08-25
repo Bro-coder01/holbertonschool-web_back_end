6c481a136f7ba7e5e28a2381d03c2d31ba59eef5
Bro-coder01 <ally185512@gmail.com>
2026-08-25 13:07:33 +0300

    done

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module that contains a function to compute the floor of a number."""
+
+
+def to_str (n: float) -> str:
+    """return string rper of a float"""
+    return str(n)
