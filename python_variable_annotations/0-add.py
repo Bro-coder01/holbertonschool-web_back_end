d57e5cf2f21a8837205d5bbb78f3f2b08bdf96a3
Bro-coder01 <ally185512@gmail.com>
2026-08-25 12:41:31 +0300

    done

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """provides mathematical operations"""
 
+
 def add(a: float, b: float) -> float:
     """Takes a float number and returns its sum value."""
     return a + b
