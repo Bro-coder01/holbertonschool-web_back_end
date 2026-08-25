ae5693d48a4f9d01023789e1d66e3f15b3d90f4a
Bro-coder01 <ally185512@gmail.com>
2026-08-25 17:35:24 +0300

    done

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module that contains a function to return string rper of a float"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """return the sum of a float list"""
+    return sum(input_list)
