b38ec29d3d466f4ffde2baec28a8b469f7f84c8c
Bro-coder01 <ally185512@gmail.com>
2026-08-26 17:47:56 +0300

    done

---
+++ 6-sum_mixed_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module that contains a function to return string rper of a float"""
+from typing import List
+
+
+def sum_mixed_list(mxd_lst: list[int|float]) -> float:
+    return sum(mxd_lst)
