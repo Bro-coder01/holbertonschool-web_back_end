b97f15687952912765b081a3c08edd4799969c6a
Bro-coder01 <ally185512@gmail.com>
2026-08-29 22:41:05 +0300

    Update 1-concurrent_coroutines.py

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -11,4 +11,3 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     """Return delays in ascending completion order."""
     tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
     return [await task for task in asyncio.as_completed(tasks)]
-    
