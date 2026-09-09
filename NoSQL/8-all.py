8288e4affdd262a5235bce0767e729056cbbe1f0
Bro-coder01 <ally185512@gmail.com>
2026-09-09 19:41:35 +0300

    done

---
+++ NoSQL/8-all.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+ function that lists all documents in a collection MongoDB.
+"""
+def list_all(mongo_collection):
+    """
+        function that lists all documents in a collection MongoDB.
+    """
+    if mongo_collection is None:
+        return []
+    return [ doc for doc in mongo_collection]
