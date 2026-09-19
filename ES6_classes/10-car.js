68b8e0bee67a0c21c98ab9065ee4359f461701c6
Bro-coder01 <ally185512@gmail.com>
2026-09-19 23:24:24 +0300

    Update 10-car.js

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,12 +1,16 @@
-class Car{
-  constructor( brand, motor, color){
-    this._brand = brand 
-    this._motor = motor
-    this._color = color
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
   }
 
-  cloneCar(){
-    return new Car();
+  static get [Symbol.species]() {
+    return this;
   }
-  
+
+  cloneCar() {
+    const Species = this.constructor[Symbol.species];
+    return new Species();
+  }
 }
